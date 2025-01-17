a565c6a641522013e10e55e182c77663a7eab63e
DevelSajjad <sinigdho01@gmail.com>
2025-01-17 20:55:54 +0600

    input data and show input data with print button.

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import Student from './Student';
 import { useState } from 'react';
 
 function App() {
-  const [data, setName] = useState('Sajjad');
-  const [email, setEmail] = useState('sajjad@gmail.com');
-  const [phone, setPhone] = useState('0171904166');
-  const [incre, setIncre] = useState(0);
-  function updateData () {
-    setName('Raja Babu');
-    setEmail('raja@gmail.com');
-    setPhone('01719041636');
+  const [data, inputData] = useState(null);
+  const [print, printData] = useState(false);
+  function getData(data) {
+    let val = data.target.value;
+    inputData(val);
   }
+
   return (
     <div className="App">
       <h1>First React App</h1>
-      <h1> {incre} </h1>
-      <h1> {data} </h1>
-      <Student name={data} email={email} phone={phone}/>
-      <button onClick={updateData} >Update</button>
-      <button onClick={()=> setIncre(incre + 1)} >Increment</button>
+
+      <p>{print ? data : null}</p>
+      <input onChange={getData}/>
+      <button onClick={()=>printData(print ? false : true)}>Print</button>
     </div>
   );
 }
