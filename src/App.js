72b27a6bb79f85a29e336067a3ed95607b780da6
DevelSajjad <sinigdho01@gmail.com>
2025-02-04 22:24:18 +0600

    hoc

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import UseMemo from './UseMemo';
 import UseRef from './UseRef';
 import ControlComponent from './ControlComponent';
 
+
 function App() {
   const [data, inputData] = useState(null);
   const [print, printData] = useState(false);
@@ -38,9 +39,27 @@ function App() {
 
       {/* <UseRef/> */}
 
-      <ControlComponent/>
+      {/* <ControlComponent/> */}
+
+      <Hoc count={Counter} />
+
+      <HocGreen count={Counter} />
     </div>
   );
+}
+
+function Hoc(props) {
+  return <h1> <props.count/> </h1>
+}
+function HocGreen(props) {
+  return <h1 style={{ backgroundColor:'green', width:300}} > <props.count/> </h1>
+}
+function Counter() {
+  const [incre, setIncre] = useState(1);
+  return <div>
+    <h1> {incre} </h1>
+    <button onClick={()=>setIncre(incre + 1)} >Increment</button>
+  </div>
 }
 
 export default App;
